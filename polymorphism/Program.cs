ccf7fe64c423d43639ed12b0e78bc5c4bebaf923
Coldani3 <p110117069@gmail.com>
2020-10-22 10:00:28 +0100

    Int and double

--- polymorphism/Program.cs
+++ polymorphism/Program.cs
@@ -6,7 +6,22 @@ namespace polymorphism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            int addedSumOfInts = AddTwoInt(9, 16);
+            Console.WriteLine("Int: " + addedSumOfInts);
+            double addedSumOfDoubles = AddTwoDoubles(7.5, 13.8);
+
+            Console.WriteLine("Double: " + addedSumOfDoubles);
+        }
+
+        static int AddTwoInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double AddTwoDoubles(double x, double y)
+        {
+            return x + y;
         }
     }
 }
