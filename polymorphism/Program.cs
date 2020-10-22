158cd2ccdbc98bb176dc8d4ba8b950e04d42b2ab
Coldani3 <p110117069@gmail.com>
2020-10-22 10:06:55 +0100

    Replace addition methods with overloaded methods

--- polymorphism/Program.cs
+++ polymorphism/Program.cs
@@ -7,19 +7,19 @@ namespace polymorphism
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            int addedSumOfInts = AddTwoInt(9, 16);
+            int addedSumOfInts = AddTwoNumbers(9, 16);
             Console.WriteLine("Int: " + addedSumOfInts);
-            double addedSumOfDoubles = AddTwoDoubles(7.5, 13.8);
+            double addedSumOfDoubles = AddTwoNumbers(7.5, 13.8);
 
             Console.WriteLine("Double: " + addedSumOfDoubles);
         }
 
-        static int AddTwoInt(int x, int y)
+        static int AddTwoNumbers(int x, int y)
         {
             return x + y;
         }
 
-        static double AddTwoDoubles(double x, double y)
+        static double AddTwoNumbers(double x, double y)
         {
             return x + y;
         }
